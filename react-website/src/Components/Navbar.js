098374a57b3ca1c084312aa26a6b923f102a354a
Teresa Kamau <51070829+tessa5@users.noreply.github.com>
2021-06-09 21:46:26 +0300

    Update Navbar.js

--- react-website/src/Components/Navbar.js
+++ react-website/src/Components/Navbar.js
@@ -2,18 +2,42 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 
 function Navbar() {
-    const[click, setClick] = useState(false);
+    const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+
+    const closeMenu = () => setClick(false)
     return (
         <>
         <nav className="navbar">
            <div className="navbar-cont">
               <Link to="/" className="nav-barLogo">LVK<i className='fab fa-affiliatetheme'/></Link> 
-              <div className='menu-icons'>
-                  <i className={click ? 'fas fa-bars' : 'fas fa-hamburger'}/>
+              <div className='menu-icons'onClick={handleClick}>
+                  <i className={click ? 'fas fa-times' : 'fas fa-hamburger'}/>
 
               </div>
+              <ul className={click ? 'nav-menu active' : 'nav-manu'}>
+                  <li className='nav-list'>
+                      <Link to='/' className='nav-link' onClick={closeMenu}>
+                          Home
+                      </Link>
+                  </li>
+                  <li className='nav-list'>
+                      <Link to='/' className='nav-link' onClick={closeMenu}>
+                         Adventure
+                      </Link>
+                  </li>
+                  <li className='nav-list'>
+                      <Link to='/' className='nav-link' onClick={closeMenu}>
+                          Explore Kenya
+                      </Link>
+                  </li>
+                  <li className='nav-list'>
+                      <Link to='/' className='nav-mobile' onClick={closeMenu}>
+                          Sign Up
+                      </Link>
+                  </li>
+              </ul>
            </div>
 
         </nav>
