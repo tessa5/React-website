dc420e5876d52d665b541c57e3efa19f7270f593
Teresa Kamau <51070829+tessa5@users.noreply.github.com>
2021-06-28 16:31:07 +0300

    styling the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,15 +7,15 @@ import {Link} from 'react-router-dom';
 function Footer(){
     return(
         
-        <div className="bg-indigo-900 p-8 flex flex-col flex justify-center items-center">
+        <div className="bg-indigo-900 p-8 ">
             <section className='flex flex-col flex justify-center items-center text-center text-white text-xl mb-4'>
                 <p className="mb-8 font-sans">
                     Join our newsletter for the best destinations in Kenya.
                 </p>
-                <div className="flex flex-col">
+                <div className="flex flex-col justify-center">
                     <form>
                         <input
-                        className="mr-4 mb-8 text-lg p-2 rounded-md flex flex-col flex justify-center"
+                        className=" footer-input mr-4 mb-8 text-lg p-2 rounded-md flex flex-col "
                         name="email"
                         type="email"
                         placeholder='Enter your Email'
@@ -25,24 +25,24 @@ function Footer(){
                 </div>
             </section>
             <section className="social">
-                <div className='social-wrap'>
-                    <div className="footer-logo">
+                <div className='social-wrap flex justify-center flex space-x-3 '>
+                    <div className="footer-logo flex-non md:flex-1 text-white text-3xl font-bold tracking-wider leading-tight">
                         <Link to='/' className='footer-logos'>
                         LVK<i className='fab fa-affiliatetheme'/>
                         </Link>
                     </div>
-                    <small className='copyrights'> LVK © 2021 </small>
-            <div class='social-icons'>
+                    <small className='copyrights flex-none md:flex-1 text-white clear-none md:clear-both'> LVK © 2021 </small>
+            <div class='social-icons min-w-auto md:min-w-0 text-xl text-white flex-non md:flex-1 flex justify-between'>
             <Link
-              class='social-icon-link facebook'
+              class='social-icon-link facebook flex justify-between p-1'
               to='/'
               target='_blank'
               aria-label='Facebook'
             >
-              <i class='fab fa-facebook-f' />
+              <i class='fab fa-facebook' />
             </Link>
             <Link
-              class='social-icon-link instagram'
+              class='social-icon-link instagram flex justify-between p-1 '
               to='/'
               target='_blank'
               aria-label='Instagram'
@@ -50,7 +50,7 @@ function Footer(){
               <i class='fab fa-instagram' />
             </Link>
             <Link
-              class='social-icon-link youtube'
+              class='social-icon-link youtube  flex justify-between p-1'
               to='/'
               target='_blank'
               aria-label='Youtube'
@@ -58,7 +58,7 @@ function Footer(){
               <i class='fab fa-youtube' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              class='social-icon-link twitter  flex justify-between p-1'
               to='/'
               target='_blank'
               aria-label='Twitter'
@@ -66,7 +66,7 @@ function Footer(){
               <i class='fab fa-twitter' />
             </Link>
             <Link
-              class='social-icon-link twitter'
+              class='social-icon-link LinkedIn flex justify-between p-1'
               to='/'
               target='_blank'
               aria-label='LinkedIn'
