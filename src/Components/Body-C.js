82d3c482eda92648ee3078aa771516495d6bf00f
Teresa Kamau <51070829+tessa5@users.noreply.github.com>
2021-07-12 21:08:36 +0300

    styling the body container

--- src/Components/Body-C.js
+++ src/Components/Body-C.js
@@ -7,14 +7,14 @@ function BodyLinks(props) {
         <>
         <li className="body-Container mb-4 flex flex-1 rounded-t-lg">
             <Link className="body-link flex flex-col " to={props.path}>
-                <figure className="h-30 w-30 m-4 border-solid border-black border-2 rounded-lg" data-category={props.label}>
+                <figure className="h-30 w-30 m-4 border-solid border-indigo-900 border-2 rounded-lg" data-category={props.label}>
                     <img 
-                        className="w-48 h-48 rounded-lg object-cover bg-gray-300 "
+                        className="w-96 h-56 rounded-lg object-cover bg-gray-300"
                         alt="magical Kenya"
                         src={props.src}
                     />
                 </figure>
-                <div className="image-info">
+                <div className="h-14 w-96 ml-4 px-16 text-lg text-center font-light text-white bg-indigo-500">
                     <h3 className="image-text">{props.text}</h3>
                 </div>
             </Link>
